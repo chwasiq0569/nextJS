526491843eb4a1801f3e3b80158b6eb16914bb39
unknown <samsunggrandprimeplus056@gmail.com>
2020-12-31 17:13:52 +0500

    jwt

---
+++ pages/api/login.ts
@@ -0,0 +1,20 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import jwt from "jsonwebtoken";
+
+const KEY = 'sajidiowuqioeklasdjasdkl';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    // console.log('REQ.BODY: ',req.body);
+    // console.log('REQ.query: ',req.query);
+    // res.statusCode = 200;
+    console.log(req.body);
+    const {username, password} = req.body;
+    // res.end("Response Stream ahs been ended here");
+    res.json({
+        token: jwt.sign({
+            username: username,
+            password: username === 'admin' && password === 'admin' 
+        }, KEY)
+    })
+}
+    
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,39 @@
+import { isTargetLikeServerless } from 'next/dist/next-server/server/config';
+import { useState } from 'react';
 import styles from './index.module.scss';
+import jwt from 'jsonwebtoken';
 
-export default function Home() {
+export default function Home(){
+  const [username ,setUsername] = useState<string>('wasiq');
+  const [password ,setPassword] = useState<string>('wasiq');
+  const [message, setMessage] = useState<string>('');
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({username, password})
+    }).then(t => t.json());
+
+    const token = res.token;
+    console.log(token)
+    if(token){
+      const json = jwt.decode(res.token) as { [key: string]: string }
+      console.log(json);
+      console.log(`Welcome ${json.username} and you are ${json.admin ? 'an admin ' : 'not admin'}`);
+             }
+    else console.log('Not Found');
+  }
   return (
-    <div>
-       <h1 className={styles.red}>Hello World<span> I am Red</span></h1>
-         {/* <style jsx>
-            {
-              `
-              .hello{
-                  color: red;
-              }
-              `
-            }
-        </style> */}
-        <h1 className='hello'>Hello World</h1>
-     
-    </div>
-    
+      <form>
+          <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <br />
+          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <br />
+          {/* <input onSubmit={submitForm} /> */}
+          <button onClick={submitForm}>Submit</button>
+      </form>
   )
 }
